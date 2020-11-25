6f30f0faa3666770ddc043e2fce5935e8dff0826
g.vyacheslav <vyacheslav.gusev@gmail.com>
2020-11-26 02:19:11 +0300

    main.tf - enable S3 at rest AES256 encryption

--- main.tf
+++ main.tf
@@ -147,6 +147,13 @@ resource "aws_s3_bucket" "chunks" {
   versioning {
     enabled = false
   }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
 
 resource "kubernetes_service_account" "this" {
